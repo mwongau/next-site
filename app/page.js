4dd24f2446ce0f33dd9a98b95f8e7064aeb75233
M Wong <wongmt15@gmail.com>
2024-12-27 02:07:21 +1100

    27-12-24

--- app/page.js
+++ app/page.js
@@ -3,21 +3,18 @@ import styles from './page.module.css'
 
 export default function Page() {
   return (
-    <div>    
-	  
-	  
+    <div>     
 	  <br />
-      
 	  <h2>Home</h2>	  
 	  
 	  <p>Welcome to my personal site. I am a researcher in computer vision and machine learning 
-	  with Ph.D in Computer Science, M.Sc and B.Sc(Hons.) in Electronic Engineering. I am also 
-	  interested in web app development. This site is built by using Next.js, JavaScript, 
-	  HTML and CSS. This site is hosted by GitHub Pages.</p>
+	  with Ph.D in Computer Science, M.Sc and B.Sc(Hons.) in Electrical and Electronic 
+	  Engineering. I am also interested in web app development. This site is built by using 
+	  Next.js, React.js, JavaScript, HTML and CSS. This site is hosted by GitHub Pages.</p>
 	  
-	  <p className={styles.footer}>This site might use cookies to improve the browsing experience of users. If you 
-	  continue to use this site, it will be assumed that you agree to the use of cookies.</p> 
-      
+	  <p className={styles.footer}>This site might use cookies to improve the browsing 
+	  experience of users. If you continue to use this site, it will be assumed that you agree 
+	  to the use of cookies.</p>     
     </div>
   );
 }
